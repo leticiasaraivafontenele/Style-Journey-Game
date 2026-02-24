0502f48bddf848acd1289b8f1a7b518c9bf8434d
leticiasaraivafontenele <licystylinson1@gmail.com>
2026-02-24 10:01:59 -0300

    master: init-front

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-5xl text-pink-500 font-bold">
+        Tailwind v4 funcionando 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
